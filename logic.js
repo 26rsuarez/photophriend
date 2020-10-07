a72bc5060ec763cb376a3ef49aba9184c462d5f2
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-10-07 13:41:47 -0500

    added weather data and 5 day weather

--- logic.js
+++ logic.js
@@ -24,20 +24,37 @@ $("button").on("click", function () {
 
 
 })
-var mykey = "515798d11075abbf042d6d0ba0edef46"
-var city = "Austin";
-queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+mykey;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(showWeather);
+//this event listener activates when a user searches for a city
+$(document).on("click", "#search-btn", function(event){
+    event.preventDefault();
+    //the city typed is stored in a variable
+    var city = $("#input-box").val().trim();
+    //clear the input-box
+    $("#input-box").val("");
+    //call openweathermap with the city
+    var mykey = "515798d11075abbf042d6d0ba0edef46"
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+mykey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(showWeather);
+    showFiveDay(city);
+})
+
 
 function showWeather(weatherData) {
-    console.log(weatherData)
+    //clear the data from previous searches
+    $("#weather-data").empty();
+
+    //display the cityname
+    var cityName = $("<p>").text(weatherData.name)
+    $("#weather-data").append(cityName)
     //temp to farenheight and fixed to one decimal point
     var temp = ((weatherData.main.temp-273.15)*9/5+32).toFixed(1);
-    console.log(temp);
+    var tempDiv = $("<div>").text("Temp: "+temp+"°F");
+    $("#weather-data").append(tempDiv)
 
     //latitude and longitude in case we need them later on
     var lat = weatherData.coord.lat;
@@ -47,17 +64,60 @@ function showWeather(weatherData) {
     var sunriseTime = new Date(weatherData.sys.sunrise*1000)
     var sunriseHour = sunriseTime.getHours();
     var sunriseMins = sunriseTime.getMinutes();
-    console.log(sunriseHour+":"+sunriseMins+"am");
+    var sunriseMins1 = String(sunriseMins).padStart(2, '0');
+    var sunriseDiv = $("<div>").text("Sunrise: "+sunriseHour+":"+sunriseMins1+" am");
+    $("#weather-data").append(sunriseDiv)
 
     //time of sunset
     var sunsetTime = new Date(weatherData.sys.sunset*1000)
     var sunsetHour = sunsetTime.getHours();
     var sunsetMins = sunsetTime.getMinutes();
-    console.log(sunsetHour-12+":"+sunsetMins+"pm");
+    var sunsetMins1 = String(sunsetMins).padStart(2, '0');
+    var sunsetDiv = $("<div>").text("Sunset: "+(sunsetHour-12)+":"+sunsetMins1+" pm");
+    $("#weather-data").append(sunsetDiv)
+
 
     //cloudy or not?
     var cloudDescription = weatherData.weather[0].description;
-    console.log(cloudDescription);
+    var cloudDiv = $("<div>").text(cloudDescription);
+    $("#weather-data").append(cloudDiv)
 
+}
+
+function showFiveDay(city) {
+    var api = "515798d11075abbf042d6d0ba0edef46";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+api;
+    $.ajax({
+        url: queryUrl,
+        type: "Get"
+    }).then(function(response){
+
+    // the object returned has 40 arrays with the day changing every 8 arrays
+    for (var i=0; i<40; i+=8) {
+        //clear the div text first
+        var divId = "#Forecast" + i;
+        $(divId).empty();
+        //the date is in the object but it needs to be parsed
+        var defaultDate = response.list[i].dt_txt;
+        var dateNeeded = defaultDate.split(" ");
+        var dateParsed = dateNeeded[0].split("-");
+        var year = dateParsed[0];
+        var month = dateParsed[1];
+        var day = dateParsed[2];
+
+        //other variables needed from the object
+        var temperatureK = response.list[i].main.temp;
+        var temperatureF = (temperatureK-273.15)*9/5+32;
+
+        // then the important variables are added to the html
+        var date = $("<p>").text(month+"/"+day+"/"+year);
+        //the icon can be obtained from the object and the website
+        var icon = $("<img>").attr("src","https://openweathermap.org/img/wn/" +response.list[i].weather[0].icon+ "@2x.png");
+
+        var temp = $("<p>").text("Temp: "+temperatureF.toFixed(1)+"°F");
+        
+        $(divId).append(date,icon,temp);
+    }
+    })
 }
 
